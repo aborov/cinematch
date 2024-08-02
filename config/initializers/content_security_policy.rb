6e333d49f50cbfa317eb564bf90bf8ac9b51598d
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-08-02 17:13:23 -0500

    Revert "fix script policy issue"

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,17 +4,17 @@
 # See the Securing Rails Applications Guide for more information:
 # https://guides.rubyonrails.org/security.html#content-security-policy-header
 
-Rails.application.configure do
-  config.content_security_policy do |policy|
+# Rails.application.configure do
+#   config.content_security_policy do |policy|
 #     policy.default_src :self, :https
 #     policy.font_src    :self, :https, :data
 #     policy.img_src     :self, :https, :data
 #     policy.object_src  :none
-    policy.script_src  :self, :https, :unsafe_inline, :unsafe_eval
+#     policy.script_src  :self, :https
 #     policy.style_src   :self, :https
 #     # Specify URI for violation reports
 #     # policy.report_uri "/csp-violation-report-endpoint"
-  end
+#   end
 #
 #   # Generate session nonces for permitted importmap, inline scripts, and inline styles.
 #   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
@@ -22,4 +22,4 @@ Rails.application.configure do
 #
 #   # Report violations without enforcing the policy.
 #   # config.content_security_policy_report_only = true
-end
+# end
