0339d12743331fb1598af4fe5ec426a58e655ff5
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-08-02 22:01:09 +0000

    more refactor to fix JS errors

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import { Turbo } from "@hotwired/turbo-rails"
 import "controllers"
-import "./survey"
+import "survey"
 import jquery from "jquery"
 import Rails from "@rails/ujs"
+import * as bootstrap from "bootstrap"
 
 Turbo.session.drive = false
 
 window.jQuery = jquery
 window.$ = jquery
+window.bootstrap = bootstrap
 
 Rails.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,7 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.0.8
-pin "jquery" # @3.7.1
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
+pin "survey", to: "survey.js"
