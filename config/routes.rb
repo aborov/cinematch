ac367a80b404647a302113b2fc0507cc372e5052
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-08-03 23:11:15 +0000

    fix for sign out by refactoring link in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: "users/registrations",
-  }
+                       registrations: "users/registrations",
+                     }
 
   root to: "recommendations#index"
 
