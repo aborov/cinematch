0590f7c192223cc0c6e8ec25665050a36e912739
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-08-04 22:07:43 +0000

    serve static assets + additional console logs for debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,14 +21,15 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  # config.public_file_server.enabled = false
+  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
-  
+
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.js_compressor = Terser.new
+  config.assets.digest = true
+  config.assets.js_compressor = :terser
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -54,11 +55,11 @@ Rails.application.configure do
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+    .tap { |logger| logger.formatter = ::Logger::Formatter.new }
     .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # "info" includes generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
