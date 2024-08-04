92f8360e41d8e6265b355d541aa81e1ffe6d4f39
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-08-04 18:27:47 +0000

    fix Uglifier error in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,11 +25,10 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
-  config.assets.js_compressor = Uglifier.new(harmony: true)
-
+  
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.digest = true
+  config.assets.compile = true
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
