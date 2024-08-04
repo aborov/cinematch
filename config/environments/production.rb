7c0766b64b9fdbc987ca255da3bf4917763b527b
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-08-04 03:28:38 +0000

    change turbo to turbolinks + assets precompile settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,8 @@ Rails.application.configure do
   config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
+  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
