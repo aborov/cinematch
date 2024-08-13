35a9df1de2c9fa523cc8610d4153e13a378977f9
Aleksei Borovikov <alex@aborovikov.com>
2024-08-13 20:43:14 +0000

    email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,11 +53,11 @@ Rails.application.configure do
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
+    .tap { |logger| logger.formatter = ::Logger::Formatter.new }
     .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # "info" includes generic and useful information about system operation, but avoids logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII). If you
@@ -72,6 +72,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "rails_template_production"
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: "cinematch.net" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtppro.zoho.com",
+    port: 465,
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
