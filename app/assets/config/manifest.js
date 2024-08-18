c50825cdefee32494c481dee9316e40e3bded2ca
Aleksei Borovikov <alex@aborovikov.com>
2024-08-18 20:05:57 +0000

    exclude unused landing scripts

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,6 +3,3 @@
 //= link_tree ../../javascript .js
 //= link application.js
 //= link_tree ../../../vendor/javascript .js
-//= link landing/388fb330498371d4935abbff11d34d4c30842ca3c4a128cdd290d29db98acb41.js
-//= link landing/01c7ccc81a1289dedbdae34afe3477f60b4bd37c9b8a15eec37e59453e6d6fd3.js
-//= link landing/939898b427480d700449229ff00dbb8a6f9f77442b532f697866e6914ab8843a.js
