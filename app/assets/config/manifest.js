9b96a91c0ed19f0db167a7229741dec5589780b4
Aleksei Borovikov <161416353+aborov@users.noreply.github.com>
2024-08-18 22:20:08 +0000

    declare missing JS script

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,3 +3,4 @@
 //= link_tree ../../javascript .js
 //= link application.js
 //= link_tree ../../../vendor/javascript .js
+//= link landing/388fb330498371d4935abbff11d34d4c30842ca3c4a128cdd290d29db98acb41.js
