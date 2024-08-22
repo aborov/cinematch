a40252305c6db8b9806d95775b0bf756c67b5f73
Aleksei Borovikov <alex@aborovikov.com>
2024-08-22 23:07:22 +0000

    personality profile calculator as service

--- app/controllers/survey_responses_controller.rb
+++ app/controllers/survey_responses_controller.rb
@@ -24,7 +24,7 @@ class SurveyResponsesController < ApplicationController
 
   def process_personality_profile(user)
     responses = user.survey_responses.includes(:survey_question)
-    personality_profile = calculate_personality_profile(responses)
+    personality_profile = PersonalityProfileCalculator.calculate(responses)
     Rails.logger.debug "Calculated Personality Profile: #{personality_profile.inspect}"
 
     user.ensure_user_preference
@@ -35,22 +35,5 @@ class SurveyResponsesController < ApplicationController
     else
       Rails.logger.error "Failed to update personality profile for user #{user.id}"
     end
-  end
-
-  def calculate_personality_profile(responses)
-    traits = %w[openness conscientiousness extraversion agreeableness neuroticism]
-    profile = {}
-
-    traits.each do |trait|
-      trait_responses = responses.select { |r| r.survey_question.question_type == trait }
-      if trait_responses.any?
-        average_score = trait_responses.map { |r| r.response.to_i }.sum / trait_responses.size.to_f
-        profile[trait] = average_score.round(2)
-      else
-        Rails.logger.warn "No responses found for trait: #{trait}"
-      end
-    end
-
-    profile
   end
 end
