e5b3e2a26feebf0bea6732ea848711c29060268d
Aleksei Borovikov <alex@aborovikov.com>
2024-11-27 01:30:33 +0000

    Revert "fix to wrong sign-in redirections"
    This reverts commit 9b138cf9aebc648913a5c157f8218c78b330cd60.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
   before_action :set_csrf_cookie
-  before_action :store_user_location!, if: :storable_location?
 
   private
 
@@ -41,21 +40,5 @@ class ApplicationController < ActionController::Base
 
   def set_watchlist_count
     @watchlist_count = current_user&.watchlist_items&.where(watched: false)&.count || 0
-  end
-
-  def storable_location?
-    request.get? && 
-    is_navigational_format? && 
-    !request.xhr? && 
-    !devise_controller? && 
-    !request.path.start_with?('/watchlist/') # Exclude all watchlist AJAX endpoints
-  end
-
-  def store_user_location!
-    store_location_for(:user, request.fullpath)
-  end
-
-  def after_sign_in_path_for(resource)
-    stored_location_for(resource) || recommendations_path
   end
 end
