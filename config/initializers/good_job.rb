828fc6c2a2664ecfd85e789ef75d7a5164924d82
Aleksei Borovikov <alex@aborovikov.com>
2025-02-20 01:11:02 +0000

    settings for async running in production

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
-  config.good_job.preserve_job_records = false
+  config.good_job.preserve_job_records = true
   config.good_job.retry_on_unhandled_error = true
   config.good_job.on_thread_error = -> (exception) { Rails.logger.error(exception) }
-  config.good_job.execution_mode = :inline
+  config.good_job.execution_mode = Rails.env.development? ? :inline : :async
   config.good_job.queues = '*'
   config.good_job.max_threads = 2
   config.good_job.poll_interval = 30 # seconds
@@ -10,8 +10,9 @@ Rails.application.configure do
   config.good_job.enable_cron = true
   config.good_job.cron = {
     fetch_content: {
-      cron: "5 9 * * *",  # Run daily at 9:05am UTC (3:05am CST)
-      class: "FetchContentJob"
+      cron: "0 9 * * *",  # Runs at 9:00am UTC (3:00am CST)
+      class: "FetchContentJob",
+      args: [{ fetch_new: true, update_existing: true, fill_missing: true }]
     }
   }
   config.good_job.cleanup_interval = 15.minutes
