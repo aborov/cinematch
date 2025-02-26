687105dc8ae4235a09c1e4870173017c0d227923
Aleksei Borovikov <alex@aborovikov.com>
2025-02-26 22:06:28 +0000

    Add Render hostnames to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,5 +105,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.hosts << "cinematch.net"
+  config.hosts << "cinematch-ywet.onrender.com"
+  config.hosts << "cinematch-jruby.onrender.com"
   config.good_job.execution_mode = :async
 end
