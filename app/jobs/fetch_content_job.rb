7662ff173bdb8d14b6f077ce1b34f98edeb30a45
Aleksei Borovikov <alex@aborovikov.com>
2025-04-02 22:11:52 +0000

    Refactor FetchContentJob to invoke Rake tasks directly for fetching, updating, and filling missing content, improving logging and removing unnecessary private methods

--- app/jobs/fetch_content_job.rb
+++ app/jobs/fetch_content_job.rb
@@ -8,15 +8,21 @@ class FetchContentJob < ApplicationJob
     Rails.logger.info "Starting FetchContentJob with options: #{options}"
     
     if options[:fetch_new] || options.empty?
-      FetchNewContentJob.perform_later
+      Rails.logger.info "Fetching new content"
+      Rake::Task['tmdb:fetch_content'].invoke
+      Rake::Task['tmdb:fetch_content'].reenable
     end
     
     if options[:update_existing] || options.empty?
-      UpdateExistingContentJob.perform_later
+      Rails.logger.info "Updating existing content"
+      Rake::Task['tmdb:update_content'].invoke
+      Rake::Task['tmdb:update_content'].reenable
     end
     
     if options[:fill_missing] || options.empty?
-      FillMissingDetailsJob.perform_later
+      Rails.logger.info "Filling missing details"
+      Rake::Task['tmdb:fill_missing_details'].invoke
+      Rake::Task['tmdb:fill_missing_details'].reenable
     end
     
     Rails.logger.info "FetchContentJob completed successfully"
@@ -24,25 +30,5 @@ class FetchContentJob < ApplicationJob
   rescue => e
     Rails.logger.error "FetchContentJob failed: #{e.message}\n#{e.backtrace.join("\n")}"
     raise e
-  end
-
-  private
-
-  def fetch_new_content
-    Rails.logger.info "Fetching new content"
-    Rake::Task['tmdb:fetch_content'].invoke
-    Rake::Task['tmdb:fetch_content'].reenable
-  end
-
-  def update_existing_content
-    Rails.logger.info "Updating existing content"
-    Rake::Task['tmdb:update_content'].invoke
-    Rake::Task['tmdb:update_content'].reenable
-  end
-
-  def fill_missing_details
-    Rails.logger.info "Filling missing details"
-    Rake::Task['tmdb:fill_missing_details'].invoke
-    Rake::Task['tmdb:fill_missing_details'].reenable
   end
 end
